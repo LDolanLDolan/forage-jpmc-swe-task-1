cf9b16517a3c785a41b3f6a52efcfdf6132d7324
LDolanLDolan <51329113+LDolanLDolan@users.noreply.github.com>
2025-07-12 14:38:30 +0100

    Update client3.py

--- client3.py
+++ client3.py
@@ -23,7 +23,10 @@ import random
 import urllib.request
 
 # Server API URLs
-QUERY = "http://localhost:8080/query?id={}"
+# QUERY = "http://localhost:8080/query?id={}"
+
+QUERY = "https://forage-jpmc-swe-task-1.onrender.com/query?id={}"
+
 
 # 500 server request
 N = 500
